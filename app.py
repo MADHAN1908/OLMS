f22cfa82bc4b57f21a445251df053bcaf4afd5c6
madhan <162275786+MADHAN1908@users.noreply.github.com>
2024-04-22 12:53:59 +0530

    s

--- app.py
+++ app.py
@@ -1,28 +1,20 @@
-from flask import Flask,g,render_template,send_file,request, redirect, url_for,session
-import subprocess
-import base64,os
-from io import BytesIO
-from datetime import datetime, timedelta
-import matplotlib
+from flask import Flask, g
+from db import get_db
+import os
 
-import matplotlib.pyplot as plt
-from db import *
-app=Flask(__name__)
-
+app = Flask(__name__)
 app.secret_key = os.urandom(24)
-app.config['UPLOAD_FOLDER'] = 'static/UPLOAD_FOLDER/'
-matplotlib.use('agg')
 
 @app.route('/cmc')
 def cmc():
     try:
-        db= get_db()
+        db = get_db()
         cur = db.cursor()
         cur.execute("SELECT 1")
-        # cur.close()
+        cur.close()
         return "sqlite3 connection is established."
     except Exception as e:
         return f"Error connecting to Sqlite3: {e}"
 
-if __name__=="__main__":
-    app.run (debug=True)    
+if __name__ == "__main__":
+    app.run(debug=True)
