19fb1337916b930270261d6abc3409875b8ff451
madhan <162275786+MADHAN1908@users.noreply.github.com>
2024-04-22 13:03:08 +0530

    as

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
-from flask import Flask, g
-from db import get_db
-import os
+from flask import Flask
 
-app = Flask(__name__)
-app.secret_key = os.urandom(24)
 
-@app.route('/cmc')
-def cmc():
-    try:
-        db = get_db()
-        cur = db.cursor()
-        cur.execute("SELECT 1")
-        cur.close()
-        return "sqlite3 connection is established."
-    except Exception as e:
-        return f"Error connecting to Sqlite3: {e}"
+app= Flask(__name__)
 
-if __name__ == "__main__":
+@app.run('/')
+def home():
+    return "welcome"
+
+
+if __name__ =="__main__":
     app.run(debug=True)
